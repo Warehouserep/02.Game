f4b1cbb2fb74368f5a164f451c3dcccef61b1a87
vladimirmironovfx <vladimir.mironov.ds@gmail.com>
2022-05-05 14:50:51 +0300

    committe_13 - update main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 //import_lib
 #include <iostream>
+#include <cstdlib>
 
 //import_header
 #include "random_value.h"
 #include "check_value.h"
 
 int main() {
-
-	//init_user_name
-	char user_name;
+	
+	//init_username
+	char username[100];
 	//init_message_for_game
 	std::cout << "Hi! Enter your name, please:" << std::endl;
-	//enter_user_name
-	std::cin >> user_name;
-	//
+	//enter_username
+	std::cin.getline(username, 100);
+			
+	//generate_random_value
 	std::cout << random_value() << std::endl;
-	//
+	//find_random_value
 	std::cout << check_value() << std::endl;
 	//
 	return 0;
